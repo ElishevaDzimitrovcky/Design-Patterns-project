bb902d5c1766665265a076e883574fe80371a133
טאוב רינה < @mbjcomp.local>
2024-03-26 15:42:56 +0200

    State design

---
+++ dpProject/state/Staged.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dpProject.state;
+
+public class Staged : ITask
+{
+    public string GetTask()
+    {
+        return "stage task";
+    }
+    public void StartTask()
+    {
+        Console.WriteLine("start stage task");
+    }
+
+    public void ChangeTask(Tasks task)
+    {
+        task.SetTask(new Commited());
+    }
+
+    public string StagedTask()
+    {
+        return "this is a staged task";
+    }
+}
---
+++ dpProject/state/Tasks.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dpProject.state;
+
+public class Tasks
+{
+    private ITask task;
+    public Tasks() 
+    {
+        task = new Draft();
+    }
+    public void StartTask()
+    {
+        while (true)
+        {
+            task.StartTask();
+            Thread.Sleep(5000);
+            task.ChangeTask(this);
+        }
+    }
+    public void SetTask(ITask task)
+    {
+        this.task = task;
+    }
+    public ITask GetTask()
+    {
+        return task;
+    }
+}
+
